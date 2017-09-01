7da5cd81920f9ac95879cc2021eddf255f3f6778
Alex Hache <ahache8@gmail.com>
2017-09-01 01:59:08 +0000

    Added Review model validation and foreign keys

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+  validates :description, presence: true
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5, only_integer: true}
+  validates :product, presence: true
+  validates :user, presence: true
 end
